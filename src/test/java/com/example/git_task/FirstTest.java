81aee50805a9ba4c9c057c75691803e13bbe40e6
MrDos180 <ilya180@yandex.ru>
2022-11-09 21:27:22 +0300

    add "world!"

--- src/test/java/com/example/git_task/FirstTest.java
+++ src/test/java/com/example/git_task/FirstTest.java
@@ -2,6 +2,6 @@ package com.example.git_task;
 
 public class FirstTest {
     public static void main(String[] args) {
-        System.out.println("Hello");
+        System.out.println("Hello world!");
     }
 }
